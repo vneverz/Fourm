41667612df2d20fdc146509dc2a8ba4f45ad58ab
vneverz <vneverz@gmail.com>
2015-04-19 17:51:07 +0800

    Add topic authors

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,9 @@ class Article < ActiveRecord::Base
 
   has_many :article_categories
   has_many :categories, :through => :article_categories
+  def authors
+    arr = self.comments.map { |c| c.user}
+    arr << self.user
+    arr.uniq
+  end
 end
